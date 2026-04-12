aabf1996b36af5ffbc32a8488565070059b96cb0
RAGNAROK_RS <8ac0c9835d463818@proton.me>
2026-04-12 19:05:37 +0200

    Phase 22: Analytics & Monitoring - Sentry error tracking, Plausible script, page view tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import { AuthModalProvider } from './auth/AuthModalProvider';
 import ErrorBoundary from './components/ErrorBoundary';
 import SecurityHeaders from './components/SecurityHeaders';
+import { Analytics, initSentry, trackPageView, SentryErrorBoundary } from './lib/analytics';
 
 export default function App() {
+  useEffect(() => {
+    initSentry();
+  }, []);
+
+  useEffect(() => {
+    const handleLocationChange = () => {
+      trackPageView(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handleLocationChange);
+    return () => window.removeEventListener('popstate', handleLocationChange);
+  }, []);
+
   return (
     <BrowserRouter>
       <SecurityHeaders />
-      <AuthModalProvider>
-        <ErrorBoundary>
-          <AppRoutes />
-        </ErrorBoundary>
-      </AuthModalProvider>
+      <Analytics />
+      <SentryErrorBoundary>
+        <AuthModalProvider>
+          <ErrorBoundary>
+            <AppRoutes />
+          </ErrorBoundary>
+        </AuthModalProvider>
+      </SentryErrorBoundary>
     </BrowserRouter>
   );
 }
